78464a3eb411e71972d3b5824543a353b9b6b084
waldo-mendoza-tierconnect-com <waldo.mendoza@tierconnect.com>
2013-05-15 19:38:29 -0400

    Add cls for expanded and collapsed state

--- app/view/Main.js
+++ app/view/Main.js
@@ -73,6 +73,7 @@ Ext.define('ATG.view.Main', {
                     xtype: 'button',
                     text: 'Games',
                     data: 'show',
+                    cls: 'atg-games-collapsed',
                     listeners: {
                         tap: me.onGamesButtonTapped
                     }
@@ -98,11 +99,15 @@ Ext.define('ATG.view.Main', {
             actionCfg = {
                 "show": {
                     toLeft: parent.element.getBox().left - 200,
-                    data: "hide"
+                    data: "hide",
+                    removeCls: 'atg-games-collapsed',
+                    addCls: 'atg-games-expanded'
                 },
                 "hide": {
                     toLeft: parent.element.getBox().left + 200,
-                    data: "show"
+                    data: "show",
+                    removeCls: 'atg-games-expanded',
+                    addCls: 'atg-games-collapsed'
                 }
             },
             config = actionCfg[button.getData()],
@@ -118,6 +123,8 @@ Ext.define('ATG.view.Main', {
 
         animation.on('animationend', function () {
             button.setData(config.data);
+            button.removeCls(config.removeCls)
+            button.addCls(config.addCls);
         });
 
         Ext.Animator.run(animation);
